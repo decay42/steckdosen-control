db775491fc26857cbd11f545d351ba8e8793a20f
Jan Pfeiffer <liberry@ymail.com>
2017-11-21 02:38:22 +0100

    clientstuff

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,3 @@
 export default {
-  systemCode: process.env.SYSTEM_CODE || '01011',
-  unitCode: process.env.UNIT_CODE || 2,
-  apiUrl: process.env.API_URL || 'http://192.168.2.99/'
+  apiUrl: 'http://192.168.2.99:3000/'
 }
--- client/src/main.js
+++ client/src/main.js
@@ -2,14 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
-
-Vue.use(require('@websanova/vue-env'))
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: { App, BootstrapVue }
 })
